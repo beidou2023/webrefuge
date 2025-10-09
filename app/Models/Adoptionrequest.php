fff5acfd0a7541c0ed714fd241879e29f4db5769
crz <ccruzstudy@gmail.com>
2025-10-09 19:20:21 -0400

    Adicion de modelos, seeders y migrations en blanco

---
+++ app/Models/Adoptionrequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Adoptionrequest extends Model
+{
+    use HasFactory;
+}
---
+++ database/seeders/RefugeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RefugeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
