f89a54f851f3b07b1490df9c98bb35eb15d31182
Carlos Cruz <ccruzstudy@gmail.com>
2025-10-10 00:02:41 -0400

    Adicion de seeder Arrivals

--- database/seeders/ArrivalSeeder.php
+++ database/seeders/ArrivalSeeder.php
@@ -2,16 +2,34 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ArrivalSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('arrivals')->insert([
+            [
+                'maleCount' => 3,
+                'femaleCount' => 2,
+                'origin' => 'MERCADO CENTRAL',
+                'notes' => 'RATAS ENCONTRADAS CERCA DE LOS CONTENEDORES.',
+                'idRefuge' => 1, // REFUGIO CENTRAL
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'maleCount' => 0,
+                'femaleCount' => 5,
+                'origin' => 'CASA ABANDONADA',
+                'notes' => 'FUE RESCATE NOCTURNO POR VOLUNTARIOS.',
+                'idRefuge' => 2, // HOGAR RATITAS
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
