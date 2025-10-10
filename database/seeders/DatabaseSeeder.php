835d235dcfbc631cb0dbcd7052339cfd95cacd11
Carlos Cruz <ccruzstudy@gmail.com>
2025-10-10 16:39:49 -0400

    agregacion del llamado a los seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,13 +7,17 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
             UserSeeder::class,
+            RefugeSeeder::class,
+            SpecialratSeeder::class,
+            AdoptionrequestSeeder::class,
+            RatSeeder::class,
+            ArrivalSeeder::class,
+            AdoptiondeliverySeeder::class,
+            RatreportSeeder::class,
         ]);
     }
 }
