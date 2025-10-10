aa8d05b0d77e3653db9df5d71fc86ceb9ffa0234
Carlos Cruz <ccruzstudy@gmail.com>
2025-10-09 23:38:30 -0400

    Adicion de seeder Refuge

--- database/seeders/RefugeSeeder.php
+++ database/seeders/RefugeSeeder.php
@@ -2,16 +2,34 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RefugeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('refuges')->insert([
+            [
+                'idManager' => 2,
+                'name' => 'REFUGIO CENTRAL LA PAZ',
+                'address' => 'AV. 6 DE AGOSTO, ZONA CENTRAL, LA PAZ',
+                'maleCount' => 12,
+                'femaleCount' => 9,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'idManager' => 3,
+                'name' => 'HOGAR DE RATITAS FELICES',
+                'address' => 'CALLE LOS CLAVELES, ZONA SUR, COCHABAMBA',
+                'maleCount' => 7,
+                'femaleCount' => 15,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
