774485050de24fdeb19c5c1d311f649f655523b4
Carlos Cruz <ccruzstudy@gmail.com>
2025-10-10 00:00:45 -0400

    Adicion de seeder Rats

--- database/seeders/RatSeeder.php
+++ database/seeders/RatSeeder.php
@@ -2,16 +2,73 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RatSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('rats')->insert([
+            // Entrega 1 - para CARLOS
+            [
+                'idAdoptiondelivery' => 1,
+                'idUser' => 4,
+                'name' => 'RAMBO',
+                'color' => 'GRIS',
+                'sex' => 'M',
+                'ageMonths' => 5,
+                'type' => 1,
+                'adoptedAt' => now(),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+                'idSpecialrat' => null,
+            ],
+            [
+                'idAdoptiondelivery' => 1,
+                'idUser' => 4,
+                'name' => 'LUCERITO',
+                'color' => 'BLANCO',
+                'sex' => 'F',
+                'ageMonths' => 4,
+                'type' => 1,
+                'adoptedAt' => now(),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+                'idSpecialrat' => null,
+            ],
+
+            // Entrega 2 - para ANA
+            [
+                'idAdoptiondelivery' => 2,
+                'idUser' => 5,
+                'name' => 'NIEVE',
+                'color' => 'BLANCO',
+                'sex' => 'F',
+                'ageMonths' => 3,
+                'type' => 1,
+                'adoptedAt' => now(),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+                'idSpecialrat' => null,
+            ],
+            [
+                'idAdoptiondelivery' => 2,
+                'idUser' => 5,
+                'name' => 'CANELA',
+                'color' => 'CAFÉ CLARO',
+                'sex' => 'F',
+                'ageMonths' => 4,
+                'type' => 1,
+                'adoptedAt' => now(),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+                'idSpecialrat' => null,
+            ],
+        ]);
     }
 }
