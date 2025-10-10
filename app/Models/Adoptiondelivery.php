e8644b243430b25fddf19dc2336e932548a9947b
Carlos Cruz <ccruzstudy@gmail.com>
2025-10-09 22:50:26 -0400

    Adicion de ADOPTIONDELIVERIES Model & Migration

--- app/Models/Adoptiondelivery.php
+++ app/Models/Adoptiondelivery.php
@@ -7,5 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Adoptiondelivery extends Model
 {
-    use HasFactory;
+    protected $table = 'adoptiondeliveries';
+
+    protected $fillable = [
+        'deliveredBy',
+        'idAdoptionrequest',
+        'maleCount',
+        'femaleCount',
+        'status',
+    ];
+
+    protected $casts = [
+        'deliveredBy' => 'integer',
+        'idAdoptionrequest' => 'integer',
+        'maleCount' => 'integer',
+        'femaleCount' => 'integer',
+        'status' => 'integer',
+    ];
+
+    public function deliveryUser()
+    {
+        return $this->belongsTo(User::class, 'deliveredBy');
+    }
+
+    public function adoptionRequest()
+    {
+        return $this->belongsTo(Adoptionrequest::class, 'idAdoptionrequest');
+    }
 }
--- database/migrations/2025_10_09_231907_create_adoptiondeliveries_table.php
+++ database/migrations/2025_10_09_231907_create_adoptiondeliveries_table.php
@@ -6,20 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('adoptiondeliveries', function (Blueprint $table) {
-            $table->id();
+            $table->mediumIncrements('id');
+            $table->unsignedMediumInteger('deliveredBy');
+            $table->unsignedMediumInteger('idAdoptionrequest');
+            $table->tinyInteger('maleCount')->default(0);
+            $table->tinyInteger('femaleCount')->default(0);
+            $table->unsignedTinyInteger('status')->default(1)->comment('0 inactive / 1 active');
             $table->timestamps();
+
+
+            $table->foreign('deliveredBy')
+                  ->references('id')
+                  ->on('users')
+                  ->onUpdate('no action')
+                  ->onDelete('no action');
+
+            $table->foreign('idAdoptionrequest')
+                  ->references('id')
+                  ->on('adoptionrequests')
+                  ->onUpdate('no action')
+                  ->onDelete('no action');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('adoptiondeliveries');
