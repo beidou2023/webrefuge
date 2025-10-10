b26ef822a31f42412c1987652511545e1ed8f14c
Carlos Cruz <ccruzstudy@gmail.com>
2025-10-09 23:57:33 -0400

    Adicion de seeder AdoptionDeliveries

--- database/seeders/AdoptiondeliverySeeder.php
+++ database/seeders/AdoptiondeliverySeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AdoptiondeliverySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('adoptiondeliveries')->insert([
+            [
+                'deliveredBy' => 2, // JAVIER PEREZ
+                'idAdoptionrequest' => 1, // solicitud de CARLOS
+                'maleCount' => 1,
+                'femaleCount' => 1,
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'deliveredBy' => 3, // LUCIA MARTINEZ
+                'idAdoptionrequest' => 2, // solicitud de ANA
+                'maleCount' => 0,
+                'femaleCount' => 2, // corregido a mínimo permitido
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
